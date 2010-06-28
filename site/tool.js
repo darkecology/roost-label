991e73dd323bb97ce230cea50069495966e9c7a1
almuallj <devnull@localhost>
2010-06-28 21:49:08 +0000

    testing svn

--- site/tool.js
+++ site/tool.js
@@ -1,5 +1,5 @@
-// Jafer Will BEGIN GLOBALS COPIED FROM LabelMe my_scripts.js
-// Will RETHINK AND REORGANIZE THESE
+// BEGIN GLOBALS COPIED FROM LabelMe my_scripts.js
+// RETHINK AND REORGANIZE THESE
 var bname;
 var bversion;
 
