6daff4250db92d32854830e1064b1eddfab1bbef
curranw <devnull@localhost>
2010-06-30 23:03:23 +0000

    Roost-28

---
+++ site/ajax/deleteSequence.php
@@ -0,0 +1,33 @@
+<?php ini_set('display_errors', 'On'); ?>
+<?php ini_set('display_startup_errors', 'On'); ?>
+<?php ini_set('error_reporting', E_ALL | E_NOTICE | E_STRICT); ?>
+<?php
+	function report_error($text) { 
+		# add other stuff you may want here 
+		$str1 = 'Error:'; //example of addon to beginning 
+		$str2 = '; filename:deleteSequence.php'; //example of addon to end 
+		die($str1.'<br />'.$text.'<br />'.$str2); 
+	}
+	if(isset($_GET["sequenceID"]) )
+	{
+	$sequenceID = $_GET["sequenceID"];
+	}else {report_error("missing sequenceID parameter");}
+	$con = mysql_connect('mysql.cs.orst.edu', 'roostdb', 'swallow');
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	mysql_select_db('roostdb', $con);
+	echo "connected";
+	$sqlDeleteCircles = "DELETE FROM Circle_Table
+							WHERE SequenceID = \"$sequenceID\"";
+			
+	$sqlDeleteSequence = "DELETE FROM Sequence_Table
+							WHERE SequenceID = \"$sequenceID\"";
+	
+	$result = mysql_query($sqlDeleteCircles);
+	$result = mysql_query($sqlDeleteSequence);
+	
+?>
+	
