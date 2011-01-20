e088174e218555a913b391989235205d1cc9d887
curranw <devnull@localhost>
2011-01-20 22:04:51 +0000

    Cookie functionality with existing users now implemented

--- site/Users.js
+++ site/Users.js
@@ -3,17 +3,36 @@ var actions = {EditRoost: 1, CreateRoost: 2};
 function User()
 {
     this.permission = 0;
-    this.userName = 0;
-    this.userID = "";
+    this.userName = "";
+    this.userID = 0;
     this.userAction = actions;
+    
+    document.getElementById("loginButton").onclick = bindEvent(this,"login");
+    document.getElementById("newUserButton").onclick = bindEvent(this,"newUser");
+    document.getElementById("logoutButton").onclick = bindEvent(this,"logout");
+    
     var cookieFound = this.getUserID();
     if(cookieFound)
     {
         //Call AJAX Login
+        var url = "ajax/user_login.php";
+	xmlhttp= new XMLHttpRequest();	
+	xmlhttp.open("POST", url, false);
+        xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xmlhttp.send("userID=" + this.userID)
+        var responseText = xmlhttp.responseText;
+        
+        var splitArray = responseText.split(';');
+        
+        if(splitArray[0] == 0)
+        {
+            alert("Getting login from cookie failed");
+            return;
+        }
+        this.userName = splitArray[1].substr(10);
+        this.permission = splitArray[2].substr(12);
         this.updateDiv();
     }
-    document.getElementById("loginButton").onclick = bindEvent(this,"login");
-    document.getElementById("newUserButton").onclick = bindEvent(this,"newUser");
 }
 User.prototype.checkPermission = function(roostUserName, action)
 {
@@ -48,15 +67,14 @@ User.prototype.checkPermission = function(roostUserName, action)
 
 User.prototype.newUser = function()
 {
-    //Call Ajax New User
     var loginUserName = document.getElementById("userName").value;
-    var password = document.getElementById("password").value;    
+    var password = document.getElementById("password").value;
+    //Call Ajax New User
     alert("Creating new user " + loginUserName);
-    
     var validNewUser = false;
     if(validNewUser)
     {
-        this.storeUserID(this.userName);
+        this.storeUserID(this.userID);
         this.updateDiv();
     }
 }
@@ -72,7 +90,7 @@ User.prototype.login = function()
     var loginPassed = false;
     if(loginPassed)
     {
-        this.storeUserID(this.userName);
+        this.storeUserID(this.userID);
         this.updateDiv();       
     }
 }
@@ -80,6 +98,9 @@ User.prototype.login = function()
 User.prototype.logout = function()
 {
     this.deleteUserID();
+    this.userID = 0;
+    this.userName = "";
+    this.permission = 0;
     this.updateDiv();
 }
 
@@ -94,6 +115,7 @@ User.prototype.getUserID = function()
         c_start=c_start + cookieName.length+1;
         var c_end=document.cookie.indexOf(";",c_start);
         if (c_end==-1) c_end=document.cookie.length;
+        this.userID = unescape(document.cookie.substring(c_start,c_end));
         alert("" + unescape(document.cookie.substring(c_start,c_end)));
         return unescape(document.cookie.substring(c_start,c_end));
         }
@@ -122,20 +144,18 @@ User.prototype.updateDiv = function()
     if(this.userName != "")
     {
         var userLoginDiv = document.getElementById("userLoginDiv");
-        document.getElementById("controlBar").removeChild(userLoginDiv);
-        userLoginDiv.style.visibility = 'hidden';
+        userLoginDiv.style.display = 'none'; 
         var userLogoutDiv = document.getElementById("userLogoutDiv");
-        document.getElementById("controlBar").appendChild(userLogoutDiv);
-        userLogoutDiv.style.visibility = 'visible';
+        userLogoutDiv.style.display = 'block';
+        var helloSpan = document.getElementById("helloSpan");
+        helloSpan.innerHTML = this.userName;
     }
     if(this.userName == "")
     {
         var userLogoutDiv = document.getElementById("userLogoutDiv");
-        document.getElementById("controlBar").removeChild(userLogoutDiv);
-        userLogoutDiv.style.visibility = 'hidden';  
+        userLogoutDiv.style.display = 'none';  
         var userLoginDiv = document.getElementById("userLoginDiv");
-        document.getElementById("controlBar").appendChild(userLoginDiv);
-        userLoginDiv.style.visibility = 'visible';    
+        userLoginDiv.style.display = 'block';    
     }
 }
 
