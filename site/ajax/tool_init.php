01e5801d848c7eab00e647bc7e7427e1ee584331
sheldon <devnull@localhost>
2011-10-01 17:46:29 +0000

    temporary fix: handle one more path
    need to change all filenames in the DB to relative paths!

--- site/ajax/tool_init.php
+++ site/ajax/tool_init.php
@@ -109,6 +109,8 @@ while($row = mysql_fetch_array($result))
     $t = $row['scan_time'];
     $url = preg_replace('/\/nfs\/spectre\/u13\/d6\/sheldon\/radar\/stations/', 'images', $row['filename']);
     $url = preg_replace('/\/nfs\/spectre\/u19\/roosts\/stations/', 'images', $url);
+    $url = preg_replace('/\/nfs\/phantom\/u5\/sheldon\/spectre\/radar\/stations/', 'images', $url);
+
     $frames_array[$t]['products'][$row['product']] = $url;
 }
 
